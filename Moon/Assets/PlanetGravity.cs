9db8668aff55c047671c1a5f7db4982928fad848
Jesse Bellingham <jbellingham91@gmail.com>
2015-03-25 11:24:27 +1300

    Initial commit.

---
+++ Moon/Assets/PlanetGravity.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetGravity : MonoBehaviour {
+
+	GameObject planet;
+	GameObject player;
+	float gravitationalAcceleration = 5.0f;
+	// Use this for initialization
+	void Start () {
+		planet = GameObject.FindGameObjectWithTag("Planet");
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Rigidbody rigidbody = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody>();
+		rigidbody.velocity += gravitationalAcceleration * Time.fixedTime * (planet.transform.position - transform.position);
+	}
+}
