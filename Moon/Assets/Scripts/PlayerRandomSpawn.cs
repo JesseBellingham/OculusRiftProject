615396f91114993642372dc7b612aed585d11926
Jesse Bellingham <jbellingham91@gmail.com>
2015-03-25 14:33:46 +1300

    Lighting and player scripting
    Added some lighting to the scene, and begun implementing a randomized
    player spawn script.

---
+++ Moon/Assets/Scripts/PlayerRandomSpawn.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerRandomSpawn : MonoBehaviour {
+
+	public GameObject[] spawnPoints = GameObject.FindGameObjectsWithTag("Spawnpoint");
+	public GameObject player = GameObject.FindGameObjectWithTag ("Player");
+
+	// Use this for initialization
+	void Start () {
+		//SpawnPlayer ();
+	
+	}
+	void SpawnPlayer(){
+		//Vector3 spawnPoint = spawnPoints [Mathf.RoundToInt(Random.Range (0, spawnPoints.Length))];
+		//Instantiate(player, spawnPoint, Quaternion.identity);
+	
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
