d541c7e1b7727f52434ad254a11b122d27eb6f02
Jesse Bellingham <jbellingham91@gmail.com>
2015-04-05 15:42:08 +1200

    Projectile scripting continued
    - Made some progress with projectile scripting
    - Added free gun model to scene

---
+++ Moon/Assets/Scripts/Gun.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+
+	public GameObject projectile;
+
+	void Start(){
+		 
+	}
+	
+	// Fire a bullet 
+	void Fire () {
+		// Create a new bullet pointing in the same direction as the gun 
+		GameObject cloneProjectile = Instantiate(projectile, transform.position, transform.rotation) as GameObject; 
+	} 
+	
+	void FixedUpdate () {
+		// Fire if the left mouse button is clicked 
+		if (Input.GetButtonDown("Fire1")) {
+			Fire();
+		} 
+	}
+}
---
+++ Moon/Assets/Scripts/Projectile.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+	public GameObject projectile;
+	public float speed;
+	public float secondsUntilDestroy = 30;
+
+	private float startTime; 
+	
+	void Start () {
+		startTime = Time.time; 
+	} 
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		Physics.IgnoreCollision (projectile.GetComponent<Collider>(), GetComponent<Collider>());
+		// Move forward 
+		this.gameObject.transform.position += speed * this.gameObject.transform.forward;
+		
+		// If the projectile has existed as long as SecondsUntilDestroy, destroy it 
+		if (Time.time - startTime >= secondsUntilDestroy) {
+			Destroy(this.gameObject);
+		} 
+	}
+	
+	void OnCollisionEnter(Collision collision) {
+		
+		// Remove the projectile from the world 
+		Destroy(this.gameObject); 
+	}
+}
