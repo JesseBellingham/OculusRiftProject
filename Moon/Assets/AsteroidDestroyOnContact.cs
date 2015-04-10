85bf4feb88098370adeae0156f2a1ff015f7ba5b
Jesse Bellingham <jbellingham91@gmail.com>
2015-04-10 21:00:39 +1200

    Asteroid scripting
    - Created randomly spawning asteroid driven by several scripts

---
+++ Moon/Assets/AsteroidDestroyOnContact.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidDestroyOnContact : MonoBehaviour {
+
+	public GameObject explosion;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col){
+		if (col.tag == "Floor") {
+			Instantiate (explosion, transform.position, transform.rotation);
+			//Destroy (this);
+		}
+	}
+}
