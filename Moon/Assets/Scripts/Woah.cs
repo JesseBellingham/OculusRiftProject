a768e1ff393fa2e1e49b05697dbcbc2f052a040b
Jesse Bellingham <jbellingham91@gmail.com>
2015-04-13 19:00:52 +1200

    Monolith
    -- Created a Monolith object easter egg
    -- Wrote a script that upon nearing the monolith, inverts gravity in the
    scene and sends the player to the secret area
    -- Added an audio clip to the monolith and scripted the audio clip to
    get louder, the closer the player gets

---
+++ Moon/Assets/Scripts/Woah.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Woah : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+		if (player.transform.localPosition.y > 600) {
+			Application.LoadLevel("ayylmao");
+		}
+	}
+
+	void OnCollisionEnter(Collision col){
+		GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+		player.GetComponent<Rigidbody> ().isKinematic = false;
+		((EnhancedFPSCharacterController)player.GetComponent<EnhancedFPSCharacterController>()).enabled = false;
+		this.GetComponent<Rigidbody> ().useGravity = true;
+		Physics.gravity = new Vector3 (0, 9.81f, 0);
+	}
+}
