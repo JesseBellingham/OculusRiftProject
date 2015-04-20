f67ed7e7463bfa760f820df57100a95caac5fe76
Jesse Bellingham <jbellingham91@gmail.com>
2015-04-20 17:28:33 +1200

    Lander scripting
    - Further attempts at scripting lander movement
    - Added wheel colliders to the lander

--- Moon/Assets/Scripts/LanderMover.cs
+++ Moon/Assets/Scripts/LanderMover.cs
@@ -4,13 +4,18 @@ using System.Collections;
 using UnityStandardAssets.CrossPlatformInput;
 using UnityStandardAssets.Utility;
 
-[RequireComponent(typeof(CharacterController))]
 public class LanderMover : MonoBehaviour
 {
 
-    public float moveSpeed = 20.0f;
-
+    public float maxSpeed = 20.0f;
+    public float turnSpeed = 20.0f;
+    public float downforce;
     public float gravity = 20.0f;
+
+    public WheelCollider frontRWheel;
+    public WheelCollider frontLWheel;
+    public WheelCollider rearRWheel;
+    public WheelCollider rearLWheel;
 
 
     // If checked, then the player can change direction while in the air
@@ -35,28 +40,65 @@ public class LanderMover : MonoBehaviour
     private Vector3 contactPoint;
     private bool playerControl = false;
     private int jumpTimer;
+    private WheelCollider[] wheelColliders = new WheelCollider[4];
+    private GameObject[] wheelMeshes = new GameObject[4];
+    private Vector3 centreOfMassOffset;
+    private Rigidbody rigidbody;
+
+    private Quaternion[] wheelMeshLocalRotations;
+
+    public float AccelInput { get; private set; }
 
     void Start()
     {
-        controller = GetComponent<CharacterController>();
+        //controller = GetComponent<CharacterController>();
         myTransform = transform;
-        speed = moveSpeed;
+        //speed = moveSpeed;
         rayDistance = controller.height * .5f + controller.radius;
         jumpTimer = antiBunnyHopFactor;
+
+        wheelMeshLocalRotations = new Quaternion[4];
+
+        for (int i = 0; i < 4; i++)
+        {
+            wheelMeshLocalRotations[i] = wheelMeshes[i].transform.localRotation;
+        }
+        wheelColliders[0].attachedRigidbody.centerOfMass = centreOfMassOffset;
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     void FixedUpdate()
     {
-        //float inputX = Input.GetAxis("Horizontal");
         float inputY = Input.GetAxis("Vertical");
+        Move(inputY);
+    }
 
-        if (grounded)
-        {      
+    // Store point that we're in contact with for use in FixedUpdate if needed
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        contactPoint = hit.point;
+    }
+
+    void Move(float accel)
+    {
+        /*for (int i = 0; i < 4; i++)
+        {
+            Quaternion quat;
+            Vector3 position;
+            wheelColliders[i].GetWorldPose(out position, out quat);
+            wheelMeshes[i].transform.position = position;
+            wheelMeshes[i].transform.rotation = quat;
+        }*/
+        AccelInput = Mathf.Clamp(accel, 0, 1);
+        Debug.Log(AccelInput);
+        ApplyDrive(AccelInput);
+        /*if (grounded)
+        {
             // Otherwise recalculate moveDirection directly from axes, adding a bit of -y to avoid bumping down inclines
-            
+
             moveDirection = new Vector3(0, -antiBumpFactor, inputY);
             moveDirection = myTransform.TransformDirection(moveDirection) * speed;
-            playerControl = true;                                
+            playerControl = true;
         }
         else
         {
@@ -66,36 +108,59 @@ public class LanderMover : MonoBehaviour
                 falling = true;
                 fallStartLevel = myTransform.position.y;
             }
-            
+
             if (playerControl)
             {
                 //moveDirection.x = inputX * speed;
                 moveDirection.z = inputY * speed;
                 moveDirection = myTransform.TransformDirection(moveDirection);
             }
-        }
+        }*/
 
+        LanderSteering();
+
+        // Apply gravity
+        moveDirection.y -= gravity * Time.deltaTime;      
+
+
+        AddDownForce();
+    }
+    private void ApplyDrive(float accel)
+    {    
+        rearLWheel.motorTorque = maxSpeed * AccelInput;
+        rearRWheel.motorTorque = maxSpeed * AccelInput;        
+    }
+
+    private void LanderSteering()
+    {
         if (Input.GetButton("VehicleRotateLeft"))
         {
-            // Input.GetButton gets held down keys -- this code block runs as long as the HoverpadRotateLeft key is held down
+            // Input.GetButton gets held down keys -- this code block runs as long as the VehicleRotateLeft key is held down
 
-            this.transform.Rotate(0, -((moveSpeed * 5) * Time.deltaTime), 0);   // Rotates the hoverpad to the left -- the number affects the speed of the rotation
+            float turn = Mathf.Clamp(Input.GetAxis("Horizontal"), -1, -1);
+            frontLWheel.steerAngle = turnSpeed * turn;
+            frontRWheel.steerAngle = turnSpeed * turn;
         }
         else if (Input.GetButton("VehicleRotateRight"))
         {
-            this.transform.Rotate(0, ((moveSpeed * 5) * Time.deltaTime), 0);
+            float turn = Mathf.Clamp(Input.GetAxis("Horizontal"), -1, -1);
+            frontLWheel.steerAngle = turnSpeed * turn;
+            frontRWheel.steerAngle = turnSpeed * turn;
         }
-
-        // Apply gravity
-        moveDirection.y -= gravity * Time.deltaTime;
-
-        // Move the controller, and set grounded true or false depending on whether we're standing on something
-        grounded = (controller.Move(moveDirection * Time.deltaTime) & CollisionFlags.Below) != 0;
     }
 
-    // Store point that we're in contact with for use in FixedUpdate if needed
-    void OnControllerColliderHit(ControllerColliderHit hit)
+    private void AddDownForce()
     {
-        contactPoint = hit.point;
+        frontLWheel.attachedRigidbody.AddForce(-transform.up * downforce *
+                                                     frontLWheel.attachedRigidbody.velocity.magnitude);
+
+        frontRWheel.attachedRigidbody.AddForce(-transform.up * downforce *
+                                                     frontRWheel.attachedRigidbody.velocity.magnitude);
+
+        rearLWheel.attachedRigidbody.AddForce(-transform.up * downforce *
+                                                     rearLWheel.attachedRigidbody.velocity.magnitude);
+
+        rearRWheel.attachedRigidbody.AddForce(-transform.up * downforce *
+                                                     rearRWheel.attachedRigidbody.velocity.magnitude);
     }
 }
