98018145f1c5964c0d4a150a09d2c1e64538bbbd
Jesse Bellingham <jbellingham91@gmail.com>
2015-05-18 14:05:46 +1200

    Second level respawn

---
+++ Moon/Assets/Scripts/PlayerRespawn.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerRespawn : MonoBehaviour {
+
+	void OnGUI () 
+	{		
+		if (Event.current.Equals (Event.KeyboardEvent ("r"))) Application.LoadLevel("Moon");		
+	}
+}
